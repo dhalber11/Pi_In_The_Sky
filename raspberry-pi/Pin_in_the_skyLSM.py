7c04263651846839aec489eea21f0cde7fdaee0a
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-12-07 10:33:34 -0500

    Update 3

---
+++ raspberry-pi/Pin_in_the_skyLSM.py
@@ -0,0 +1,33 @@
+import time
+import board
+import busio
+import adafruit_mpl3115a2
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+
+sda_pin = board.GP14 #Sets the sda pin
+scl_pin = board.GP15 #Sets the scl pin
+i2c = busio.I2C(scl_pin, sda_pin) # Connects the sda and scl to i2c
+
+sensor = LSM6DSOX(i2c, address)
+
+altimeter = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
+
+startAlt = altimeter.altitude
+releaseAlt = 3
+dropped = False
+
+with open("/data.csv", "a") as datalog: # When Data Mode is Active
+    while True:
+        displaceAlt = altimeter.altitude - startAlt
+        if displaceAlt >= releaseAlt:
+            dropped = True
+        while dropped == True:
+            time_elapsed = time.monotonic()
+            Xaccel = sensor.acceleration[0] # Reads the X acceleration
+            Yaccel = sensor.acceleration[1]
+            Zaccel = sensor.acceleration[2]
+            Alt = altimeter.altitude
+
+            datalog.write(f"{time_elapsed},{Xaccel},{Yaccel},{Zaccel},{Alt}\n") # Put the data into a chart
+            datalog.flush() # Save the data
+            time.sleep(.1)
