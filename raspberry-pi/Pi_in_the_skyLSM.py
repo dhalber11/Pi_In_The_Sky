45b4d39e235d7c99279189afff0eb27f0b1db720
Grant <91094422+ggastin30@users.noreply.github.com>
2024-05-08 09:34:00 -0400

    Update Pi_in_the_skyLSM.py

--- raspberry-pi/Pi_in_the_skyLSM.py
+++ raspberry-pi/Pi_in_the_skyLSM.py
@@ -3,48 +3,45 @@ import time
 import board
 import busio
 import adafruit_mpl3115a2
+import pwmio
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+from adafruit_motor import servo
 
 sda_pin = board.GP14 #Sets the sda pin
 scl_pin = board.GP15 #Sets the scl pin
 i2c = busio.I2C(scl_pin, sda_pin) # Connects the sda and scl to i2c
 
+sensor = LSM6DSOX(i2c, address=0x6a) # Set up the LSM
+altimeter = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60) # Set up the altimeter
 
-sensor = LSM6DSOX(i2c, address=0x6a)
-altimeter = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
+pwm = pwmio.PWMOut(board.GP4, duty_cycle=2 ** 15, frequency=50) # create a PWMOut object on Pin A2.
+my_servo = servo.Servo(pwm, min_pulse=500, max_pulse=2500) # Define the servo
 
-startAlt = altimeter.altitude
-releaseAlt = 3
+startAlt = altimeter.altitude #Find Ground Zero
+releaseAlt = 20 # In meters
 dropped = False
 
 with open("/data.csv", "a") as datalog: # When Data Mode is Active
-    #datalog.write("Time(s),X-accel,Y-accel,Z-accel,X-tilt,Y-tilt,Z-tilt,Altitude(m)\n") # Put the data into a chart
-    datalog.write(f"time_elapsed,X-accel,Y-accel,Z-accel,Xvelocity,Yvelocity,Zvelocity,Alt")
-    datalog.flush()
-    while True:
-        time_elapsed = time.monotonic()
-        #datalog.write(f"{time_elapsed}, , , , , , ,{altimeter.altitude}\n")
-        #datalog.flush() # Save the data
-        displaceAlt = altimeter.altitude - startAlt
-        if displaceAlt >= releaseAlt:
-            dropped = True
+    datalog.write("Time(s),X-accel,Y-accel,Z-accel,Altitude(m)\n") # Set headers for the graph
+    datalog.flush() # Put the data into a chart
+    my_servo.angle = 180  # Locks with drone
 
+    while True:
+
+        time_elapsed = time.monotonic()
+        displaceAlt = altimeter.altitude - startAlt # Find distance from ground
+        if displaceAlt >= releaseAlt: # When drop height is reached
+            my_servo.angle = 120 # Bombs awayyyyy
+            dropped = True
+            
         while dropped == True:
             time_elapsed = time.monotonic()
-            Xa = sensor.acceleration[0] # Reads the X acceleration
-            Ya = sensor.acceleration[1]
-            Za = sensor.acceleration[2]
-            Alt = altimeter.altitude
-            Xg = sensor.gyro[0]
-            Yg = sensor.gyro[1]
-            Zg = sensor.gyro[2]
 
-            time.sleep(.2)
+            Xa,Ya,Za = sensor.acceleration # Sensor readings
+            Alt = altimeter.altitude # Altitude readings
+            Xg,Yg,Zg = sensor.gyro
+          
+            time.sleep(.00001) # So there isn't an overload
 
-            Xv = ((sensor.acceleration[0] + Xa)/2) * (time.monotonic() - time_elapsed)
-            Yv = ((sensor.acceleration[1] + Ya)/2) * (time.monotonic() - time_elapsed)
-            Zv = ((sensor.acceleration[2] + Za)/2) * (time.monotonic() - time_elapsed)
-
-            #datalog.write(f"{time_elapsed},{Xa},{Ya},{Za},{Xg},{Yg},{Zg},{Xv},{Yv},{Zv},{Alt}\n") # Put the data into a chart
-            datalog.write(f"{time_elapsed},{Xa},{Ya},{Za},{Xv},{Yv},{Zv}\n")
+            datalog.write(f"{time_elapsed},{Xa},{Ya},{Za},{Alt}\n") # Compile the data
             datalog.flush() # Save the data
